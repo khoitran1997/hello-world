958093762fec4d140c470272f5d7533339744ba6
khoitran1997 <69920518+khoitran1997@users.noreply.github.com>
2020-08-19 14:27:22 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************
+*Hello.java
+***********************
+*Khoi Tran
+*8/19/2020
+*CMSC 255/ Section 002
+**********************/
+
+public class Hello {
+
+	public static void main(String[] args){
+
+		System.out.println("Khoi Tran");
+		System.out.println("My Name In Code!");
+		System.out.println("This project will display 5 output lines of my name. The way the name will be creative.");
+		System.out.println("08/19/2020");
+		System.out.println("CMSC 255 Section 002");
+		System.out.println();
+	}
+}
